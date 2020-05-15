b6811f6711d73da7bd7d8eba0a4f58c4f95e961e
cuicui <1606947588@qq.com>
2020-05-15 11:15:59 +0800

    整理一个空架子出来

--- src/router/index.js
+++ src/router/index.js
@@ -10,119 +10,23 @@ export const constantRouterMap = [
     component: Layout,
     // redirect: '/',
     hidden: true,
-    meta: { title: 'echarts实例' },
+    meta: { title: '空架子' },
     children: [
       {
         path: '/',
-        name: '柱状图',
-        component: resolve => require(['@/view/biz/echarts/one'], resolve),
-        meta: { title: '柱状图' },
+        name: 'demo01',
+        component: resolve => require(['@/view/biz//demo01'], resolve),
+        meta: { title: 'demo01' },
         // redirect: '/',
       },
       {
         path: '/two',
-        name: '折线图',
-        component: resolve => require(['@/view/biz/echarts/two'], resolve),
-        meta: { title: '折线图' },
-      },
-      {
-        path: '/three',
-        name: '圆形图',
-        component: resolve => require(['@/view/biz/echarts/three'], resolve),
-        meta: { title: '圆形图' },
-      },
-      {
-        path: '/ranjin',
-        name: '燃尽图',
-        component: resolve => require(['@/view/biz/echarts/ranjin'], resolve),
-        meta: { title: '燃尽图' },
-      },
+        name: 'demo02',
+        component: resolve => require(['@/view/biz/demo02'], resolve),
+        meta: { title: 'demo02' },
+      }
     ]
-  },
-  {
-    path: '/table',
-    name: 'elementUI表格中的实例',
-    component: Layout,
-    // redirect: '/',
-    meta: { title: 'elementUI表格中的实例' },
-    children: [
-      {
-        path: '/burnoutChart',
-        name: '燃尽图/步骤条',
-        component: resolve => require(['@/view/biz/table/table-burnout-chart'], resolve),
-        meta: { title: '燃尽图/步骤条' },
-      },
-      {
-        path: '/tableCol',
-        name: 'tree表格动态合并',
-        component: resolve => require(['@/view/biz/table/table'], resolve),
-        meta: { title: 'tree表格动态合并' },
-      },
-      {
-        path: '/tableAll',
-        name: 'tree表格合计总数',
-        component: resolve => require(['@/view/biz/table/table-all'], resolve),
-        meta: { title: 'tree表格合计总数' },
-      },
-    ]
-  },
-  {
-    path: '/tiny',
-    name: '富文本实例',
-    component: Layout,
-    // redirect: '/',
-    meta: { title: '富文本实例' },
-    children: [
-      {
-        path: '/tinymce',
-        name: '富文本',
-        component: resolve => require(['@/view/biz/tinymce/tinymce'], resolve),
-        meta: { title: '富文本' },
-      },
-    ]
-  },
-  {
-    path: '/components',
-    name: '封装组件',
-    component: Layout,
-    // redirect: '/',
-    meta: { title: '封装组件' },
-    children: [
-      {
-        path: '/Demo01',
-        name: '封装组件第一篇',
-        component: resolve => require(['@/view/biz/useComponents/Demo01'], resolve),
-        meta: { title: '封装组件第一篇' },
-      },
-      {
-        path: '/Demo02',
-        name: '封装组件第二篇',
-        component: resolve => require(['@/view/biz/useComponents/Demo02'], resolve),
-        meta: { title: '表单' },
-      },
-    ]
-  },
-  {
-    path: '/communication',
-    name: '组件通信',
-    component: Layout,
-    // redirect: '/',
-    meta: { title: '组件通信' },
-    children: [
-      {
-        path: '/patentDemo01',
-        name: '父子组件通信',
-        component: resolve => require(['@/view/biz/componentCommunication/parent-son-demo01/parent'], resolve),
-        meta: { title: '父子组件通信' },
-      },
-      {
-        path: '/patentDemo02',
-        name: 'patentDemo02',
-        component: resolve => require(['@/view/biz/componentCommunication/parent-son-demo02/parent'], resolve),
-        meta: { title: 'dispatch 和 broadcast' },
-      },
-    ]
-  },
+  }
 ]
 
 export default new Router({
