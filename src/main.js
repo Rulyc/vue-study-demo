3184d56c2d14e7fbfb87e713fae20615b669e2fa
cuicui <1606947588@qq.com>
2020-05-19 09:35:14 +0800

    安装npm集成的插件，并引入使用

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import 'element-ui/lib/theme-chalk/index.css'
 import './assets/style/common.scss'
 import showdown from 'showdown'// 引入md文件
 import 'prismjs/themes/prism.css';
+Vue.use(showdown)
 Vue.use(ElementUI)
 Vue.config.productionTip = false
 process.env.Mock && require('./mock.js')
