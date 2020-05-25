bb1da438b4408d72f1e6554780c87ea8c7e2ab6f
cuicui <1606947588@qq.com>
2020-05-25 12:24:33 +0800

    解决IE11下promise报错打不开问题

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import './assets/style/common.scss'
-
+import "babel-polyfill" // 解决IE11下报promise未定义问题  安装命令：npm install babel-polyfill --save
 Vue.use(ElementUI)
 Vue.config.productionTip = false
 process.env.Mock && require('./mock.js')
