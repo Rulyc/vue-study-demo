c8836dbf54f06127ffe38da98d6b104fee7053bc
cuicui <1606947588@qq.com>
2020-05-27 17:38:58 +0800

    对webpack配置进行深入理解

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -8,20 +8,30 @@ function resolve (dir) {
   return path.join(__dirname, '..', dir)
 }
 
-
+/**
+ *  Plugins: 配置扩展插件
+ *  DevServer: 配置DevServer
+ * */
 
 module.exports = {
+  // webpack在寻找相对路径的文件时会以context为根目录，context默认为执行启动webpack是所在的当前工作目录
   context: path.resolve(__dirname, '../'),
+  // 配置模块的入口（必填），可抽象理解为输入，webpack执行构建的第一步从入口开始，搜寻及递归解析出所有入口依赖的模块
   entry: {
     app: './src/main.js'
   },
+  // 配置如何输出最终想要的代码
   output: {
+    // 配置输出文件存放在本地的目录，必须string类型的据绝对路径 通常通过node.js的path模块去获取绝对路径path: path . resolve( dirname,’dist [hash ]’)
     path: config.build.assetsRoot,
+    // 配置输出文件的名字，为string类型；有多个chunk要输出的时候，可以采用filename: '[name].js'
     filename: '[name].js',
+    // publicPath配置发布到线上资源的URL前缀，为string类型，使用相对路径； 使用该配置项时要小心，稍有不慎将导致资源加载 404 误。
     publicPath: process.env.NODE_ENV === 'production'
       ? config.build.assetsPublicPath
       : config.dev.assetsPublicPath
   },
+  // 配置寻找模块的规则
   resolve: {
     extensions: ['.js', '.vue', '.json'],
     alias: {
@@ -29,6 +39,7 @@ module.exports = {
       '@': resolve('src'),
     }
   },
+  // 配置处理模块的规则
   module: {
     rules: [
       {
